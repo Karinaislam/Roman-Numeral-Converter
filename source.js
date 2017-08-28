7569dc004818b9609707e2938b52e97142a9748c
Karina.I <badhon12karina@gmail.com>
2017-08-28 16:04:02 -0500

    Add files via upload

---
+++ source.js
@@ -0,0 +1,44 @@
+function input(){
+
+  var num = document.getElementById('input').value;
+  var output = convertToRoman(num);
+  document.getElementById('output').innerHTML= output;
+}
+
+
+
+
+
+function convertToRoman(num) {
+  var romans = [
+    ["I", "V"], 
+    ["X", "L"], 
+    ["C", "D"], 
+    ["M"]       
+  ],
+      digits = num.toString()
+        .split('')
+        .reverse()
+        .map(function(item, index) {
+          return parseInt(item);
+        }),
+      numeral = "";
+  
+  
+  for (var i=0; i<digits.length; i++) {
+    
+    numeral = romans[i][0].repeat(digits[i]) + numeral;
+    
+    if (romans[i][1]) {
+      numeral = numeral
+        
+        .replace(romans[i][0].repeat(5), romans[i][1])
+        
+        .replace(romans[i][1] + romans[i][0].repeat(4), romans[i][0] + romans[i+1][0])
+        
+        .replace(romans[i][0].repeat(4), romans[i][0] + romans[i][1]);
+    }
+  }
+  
+  return numeral;
+}
